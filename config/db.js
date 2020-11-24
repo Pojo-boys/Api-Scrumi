6c86efccb324f8bc3c133a6f870c8726baf6adb5
ehdavison <elijahdavison2@gmail.com>
2020-11-24 15:36:44 -0600

    The for real set up complete

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'Api-Scrummi'
+const mongooseBaseName = 'Api-Scrumi'
 
 // create the mongodb uri for development and test
 const database = {
