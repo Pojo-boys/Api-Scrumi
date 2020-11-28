d03411993c718efa45c62198d63a72e7a4378d78
kdoxsey <kdoxse@gmail.com>
2020-11-28 16:07:07 -0500

    fixed indentations in task model

--- app/models/task.js
+++ app/models/task.js
@@ -1,26 +1,26 @@
-// Import mongoose for the schema 
+// Import mongoose for the schema
 const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true
-	},
-	description: {
-		type: String,
-		required: true
-	},
-	isChecked: {
-		type: Boolean,
-		required: true
-	},
-	owner: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	}
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  isChecked: {
+    type: Boolean,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 }, {
-	timestamps: true
+  timestamps: true
 })
 
 module.exports = mongoose.model('Task', taskSchema)
